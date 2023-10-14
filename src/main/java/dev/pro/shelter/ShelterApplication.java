994793b85cbe33a0f78458b161892c3705ce22fc
Cephey <msvprosto@mail.ru>
2023-10-14 19:20:49 +0400

    Add config files

---
+++ src/main/java/dev/pro/shelter/ShelterApplication.java
@@ -0,0 +1,10 @@
+package dev.pro.shelter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ShelterApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterApplication.class, args);
+	}
+
+}
