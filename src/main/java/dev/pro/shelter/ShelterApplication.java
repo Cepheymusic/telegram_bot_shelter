d82a08d1f73380c3f288f502920e42f08ddd95bb
Cephey <msvprosto@mail.ru>
2023-10-14 19:51:36 +0400

    Add project structure

--- src/main/java/dev/pro/shelter/ShelterApplication.java
+++ src/main/java/dev/pro/shelter/ShelterApplication.java
@@ -1,7 +1,10 @@
 package dev.pro.shelter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
+@EnableScheduling
 public class ShelterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShelterApplication.class, args);
