197f22d722ec13dc779c78cc18ab318fd4311a6b
Cephey <msvprosto@mail.ru>
2023-10-21 00:06:36 +0400

    Добавили классы контроллера и сервиса репозитория

--- src/main/java/dev/pro/shelter/service/DogServiceImpl.java
+++ src/main/java/dev/pro/shelter/service/DogServiceImpl.java
@@ -1,4 +1,32 @@
 package dev.pro.shelter.service;
 
-public class DogServiceImpl {
+import dev.pro.shelter.model.Pet;
+import dev.pro.shelter.repository.DogRepository;
+
+public class DogServiceImpl implements PetService {
+    private final DogRepository dogRepository;
+
+    public DogServiceImpl(DogRepository dogRepository) {
+        this.dogRepository = dogRepository;
+    }
+
+    @Override
+    public Pet createPet(Pet pet) {
+        return null;
+    }
+
+    @Override
+    public Pet updatePet(Long id) {
+        return null;
+    }
+
+    @Override
+    public Pet deletePet(Long id) {
+        return null;
+    }
+
+    @Override
+    public Pet readPet(Long id) {
+        return null;
+    }
 }
