cf09eb2bd3f64b8fc47cb1dc85e7b38020377f25
Cephey <msvprosto@mail.ru>
2023-10-27 10:35:22 +0400

    Добавлены кнопки выбора приюта

--- src/main/java/dev/pro/shelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/dev/pro/shelter/listener/TelegramBotUpdatesListener.java
@@ -4,6 +4,8 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -31,10 +34,20 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             logger.info("Processing update: {}", update);
             if (update.message().text().equals("/start")) {
                 long idChat = update.message().chat().id();
-                SendMessage message = new SendMessage(idChat, "You send /start");
+                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup(
+                        new InlineKeyboardButton("Приют для собак").callbackData("dogShelter"),
+                        new InlineKeyboardButton("Приют для кошек").callbackData("catShelter")
+                );
+                SendMessage message = new SendMessage(idChat, "Добро пожаловать в наш мир кошечек и собачек. Выберите приют")
+                        .replyMarkup(markupInline);
                 telegramBot.execute(message);
             }
+//            else if (update.callbackQuery().data().equals("dogShelter")) {
+//
+//            }
+
         });
+
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 }
