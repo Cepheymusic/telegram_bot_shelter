82579e471ae86124308803050dc32d04c6c3071d
Cepheymusic <134048392+Cepheymusic@users.noreply.github.com>
2023-10-27 10:22:43 +0300

    Feature 1 (#8)
    * Dog Module is done.
    
    * add model Cat
    
    * correct model Cat
    
    * model pet
    
    * Добавили классы контроллера и сервиса
    
    * Добавили классы контроллера и сервиса репозитория
    
    * Рефакторинг
    
    * Добавлены контроллеры + сервисы животных
    
    * Добавлены кнопки выбора приюта
    
    ---------
    
    Co-authored-by: SONEKT <shulmanlevgen@gmail.com>
    Co-authored-by: Larisa <laserova@yandex.ru>
    Co-authored-by: LSEkb <134659238+LSEkb@users.noreply.github.com>

--- src/main/java/dev/pro/shelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/dev/pro/shelter/listener/TelegramBotUpdatesListener.java
@@ -4,84 +4,61 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
-// <<<<<<< feature-2
-// import dev.pro.shelter.service.impl.UsersServiceImpl;
-// =======
-// >>>>>>> dev
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
+// // <<<<<<< feature-2
+// // import dev.pro.shelter.service.impl.UsersServiceImpl;
+// // =======
+// // >>>>>>> dev
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
+// import javax.annotation.PostConstruct;
+// import java.util.ArrayList;
+// import java.util.List;
 
-@Service
-public class TelegramBotUpdatesListener implements UpdatesListener {
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-// <<<<<<< feature-2
-//     private final TelegramBot telegramBot;
-//     private UsersServiceImpl userService;
+// @Service
+// public class TelegramBotUpdatesListener implements UpdatesListener {
+//     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+// // <<<<<<< feature-2
+// //     private final TelegramBot telegramBot;
+// //     private UsersServiceImpl userService;
 
-//     public TelegramBotUpdatesListener(TelegramBot telegramBot, UsersServiceImpl userService) {
-//         this.telegramBot = telegramBot;
-//         this.userService = userService;
+// //     public TelegramBotUpdatesListener(TelegramBot telegramBot, UsersServiceImpl userService) {
+// //         this.telegramBot = telegramBot;
+// //         this.userService = userService;
+// //     }
+
+// // //    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+// // //        this.telegramBot = telegramBot;
+// // //    }
+// // =======
+
+// //     private final TelegramBot telegramBot;
+// //     public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+// //         this.telegramBot = telegramBot;
+// //     }
+// // >>>>>>> dev
+
+//     @PostConstruct
+//     public void init() {
+//         telegramBot.setUpdatesListener(this);
 //     }
 
-// //    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
-// //        this.telegramBot = telegramBot;
-// //    }
-// =======
-
-//     private final TelegramBot telegramBot;
-//     public TelegramBotUpdatesListener(TelegramBot telegramBot) {
-//         this.telegramBot = telegramBot;
-//     }
-// >>>>>>> dev
-
-    @PostConstruct
-    public void init() {
-        telegramBot.setUpdatesListener(this);
-    }
-
-    @Override
-    public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-// <<<<<<< feature-2
-//             // Process your updates here
-//             String message = update.message().text();
-//             if (update.message() == null) {
-//                 logger.error("Message is null");
-//                 return;
-//             } else {
-//                 logger.info("Processing message: \"{}\"", message);
+//     @Override
+//     public int process(List<Update> updates) {
+//         updates.forEach(update -> {
+//             logger.info("Processing update: {}", update);
+// ?
 //             }
+// //            else if (update.callbackQuery().data().equals("dogShelter")) {
+// //
+// //            }
 
-//             if (message==null)
-//             {return;}
-
-//             if (message.equals("/start")) {
-//                 long idChat = update.message().chat().id();
-//                 if (userService.existsUserByChatId(idChat)) {
-//                     replyToMessage(update, "Приветствуем старого знакомого! Выбери, пожалуйста, приют для " +
-//                             "продолжения работы");
-//                 } else {
-//                     replyToMessage(update, "Приветствуем нового гостя! Наш бот поможет тебе выбрать друга из приюта. " +
-//                             "Для продолжения работы выбери, пожалуйста, приют");
-//                     userService.createUser(update.message().chat().id());
-//                 }
-
-// //                SendMessage message = new SendMessage(idChat, "You send /start");
-// //                telegramBot.execute(message);
-// =======
-//             if (update.message().text().equals("/start")) {
-//                 long idChat = update.message().chat().id();
-//                 SendMessage message = new SendMessage(idChat, "You send /start");
-//                 telegramBot.execute(message);
-// >>>>>>> dev
-            }
         });
+
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
